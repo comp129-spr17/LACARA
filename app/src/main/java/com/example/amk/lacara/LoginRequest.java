3adf7d9fb6f2c718ba21cb62f7c3dd7900c74ccf
aungmkyaw <a_kyaw@u.pacific.edu>
2017-02-20 17:29:15 -0800

    Working login/Register

--- app/src/main/java/com/example/amk/lacara/LoginRequest.java
+++ app/src/main/java/com/example/amk/lacara/LoginRequest.java
@@ -1,5 +1,6 @@
 package com.example.amk.lacara;
 
+import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.toolbox.StringRequest;
 
@@ -10,18 +11,17 @@ import java.util.Map;
  * Created by AMK on 2/20/2017.
  */
 
-public class RegisterRequest extends StringRequest {
+public class LoginRequest extends StringRequest {
 
-    private static final String regReqUrl = "http://suivre.hostoi.com/Register.php";
+    private static final String regReqUrl = "http://suivre.hostoi.com/Login.php";
     private Map<String, String> params;
 
-    public RegisterRequest(String useremail, String password, Response.Listener<String> listener) {
-        super(Method.POST, regReqUrl, listener, null);
+    public LoginRequest(String useremail, String password, Response.Listener<String> listener) {
+        super(Request.Method.POST, regReqUrl, listener, null);
         params = new HashMap<>();
         params.put("useremail", useremail);
         params.put("password", password);
     }
-
     @Override
     public Map<String, String> getParams() {
         return params;
--- app/src/main/java/com/example/amk/lacara/RegisterActivity.java
+++ app/src/main/java/com/example/amk/lacara/RegisterActivity.java
@@ -31,35 +31,46 @@ public class RegisterActivity extends AppCompatActivity {
         btRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final String email = entEmail.getText().toString();
+                final String useremail = entEmail.getText().toString();
                 final String password = entPass.getText().toString();
+                final String password2 = entConPass.getText().toString();
 
-                Response.Listener<String> resListener = new Response.Listener<String>(){
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONObject jsonRes = new JSONObject(response);
-                            boolean success = jsonRes.getBoolean("Created!");
+               // if (entPass == entConPass) {
+                    Response.Listener<String> resListener = new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            try {
+                                JSONObject jsonRes = new JSONObject(response);
+                                boolean success = jsonRes.getBoolean("success");
 
-                            if (success) {
-                                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
-                                RegisterActivity.this.startActivity(intent);
-                            } else {
-                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
-                                builder.setMessage("Registration failed!")
-                                        .setNegativeButton("Please try again!", null)
-                                        .create()
-                                        .show();
-                            }
-                        }catch (JSONException e) {
+                                if (success) {
+                                    Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                                    RegisterActivity.this.startActivity(intent);
+                                } else {
+                                    AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                    builder.setMessage("Registration failed!")
+                                            .setNegativeButton("Please try again!", null)
+                                            .create()
+                                            .show();
+                                }
+                            } catch (JSONException e) {
                                 e.printStackTrace();
                             }
                         }
-                };
+                    };
 
-                RegisterRequest registerRequest = new RegisterRequest(email, password, resListener);
-                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                    RegisterRequest registerRequest = new RegisterRequest(useremail, password, resListener);
+                    RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                    queue.add(registerRequest);
 
+              // }
+               /*else {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                    builder.setMessage("Password does not match!")
+                            .setNegativeButton("Please try again!", null)
+                            .create()
+                            .show();
+                }*/
             }
         });
 
