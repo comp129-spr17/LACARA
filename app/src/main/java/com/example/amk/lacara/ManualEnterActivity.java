b4e95db782a80183face6624188a1c9b65ffea5e
aungmkyaw <a_kyaw@u.pacific.edu>
2017-02-27 23:10:26 -0800

    Added left and righ swipe

---
+++ app/src/main/java/com/example/amk/lacara/ManualEnterActivity.java
@@ -0,0 +1,19 @@
+package com.example.amk.lacara;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ManualEnterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manualenter);
+    }
+}
