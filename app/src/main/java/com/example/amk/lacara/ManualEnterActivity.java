a2fef2b9f8d8a86590385bd916360120189b7d88
L.J. Imutan <l_imutan1@u.pacific.edu>
2017-03-07 08:29:26 -0800

    Created Manual Entry Page. Does not store any data yet.

--- app/src/main/java/com/example/amk/lacara/ManualEnterActivity.java
+++ app/src/main/java/com/example/amk/lacara/ManualEnterActivity.java
@@ -2,6 +2,12 @@ package com.example.amk.lacara;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.toolbox.StringRequest;
@@ -15,5 +21,22 @@ public class ManualEnterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manualenter);
+    }
+    private EditText item;
+    private EditText location;
+    private EditText price;
+
+    public void buttonOnClick(View v) {
+        Button button=(Button) v;
+        //Button price=(Button) v;
+        //Button location=(Button) v;
+        item = (EditText) findViewById(R.id.itemName);
+        location = (EditText) findViewById(R.id.locationInput);
+        price = (EditText) findViewById(R.id.priceInput);
+        Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.TOP| Gravity.LEFT, 0, 0);
+        toast.makeText(ManualEnterActivity.this, item.getText(), toast.LENGTH_SHORT).show();
+        toast.makeText(ManualEnterActivity.this, location.getText(), toast.LENGTH_SHORT).show();
+        toast.makeText(ManualEnterActivity.this, price.getText(), toast.LENGTH_SHORT).show();
     }
 }
