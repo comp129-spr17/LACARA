ee86b289f8e4f718687a0f35f29fc2f785354fc9
Ming Yan Shu <Ming Yan Shu>
2017-03-09 15:40:21 -0800

    Add relative layout to calendar

--- app/src/main/java/com/example/amk/lacara/CalendarActivity.java
+++ app/src/main/java/com/example/amk/lacara/CalendarActivity.java
@@ -14,7 +14,6 @@ import java.util.Calendar;
 
 public class CalendarActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
@@ -33,7 +32,5 @@ public class CalendarActivity extends AppCompatActivity {
                 Toast.makeText(CalendarActivity.this,""+date,Toast.LENGTH_SHORT).show();
             }
         });
-
-
     }
 }
