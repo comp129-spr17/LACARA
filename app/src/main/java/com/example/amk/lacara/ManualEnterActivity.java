568d68c5521bc78f7fae4dcab54f6ae295921b37
L.J. Imutan <l_imutan1@u.pacific.edu>
2017-03-16 16:59:23 -0700

    Added Manual Entry save to text file

--- app/src/main/java/com/example/amk/lacara/ManualEnterActivity.java
+++ app/src/main/java/com/example/amk/lacara/ManualEnterActivity.java
@@ -1,5 +1,7 @@
 package com.example.amk.lacara;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -7,16 +9,25 @@ import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.toolbox.StringRequest;
 
+import org.w3c.dom.Text;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class ManualEnterActivity extends AppCompatActivity {
+
+     EditText item;
+     EditText location;
+     EditText price;
+     TextView areaDisplay;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,24 +35,44 @@ public class ManualEnterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_manualenter);
         Toolbar ManualToolbar = (Toolbar) findViewById(R.id.ManualTB);
         setSupportActionBar(ManualToolbar);
-    }
 
-    private EditText item;
-    private EditText location;
-    private EditText price;
-
-    public void buttonOnClick(View v) {
-        Button button=(Button) v;
-        //Button price=(Button) v;
-        //Button location=(Button) v;
         item = (EditText) findViewById(R.id.itemName);
         location = (EditText) findViewById(R.id.locationInput);
         price = (EditText) findViewById(R.id.priceInput);
-        Toast toast = new Toast(getApplicationContext());
-        toast.setGravity(Gravity.TOP| Gravity.LEFT, 0, 0);
-        toast.makeText(ManualEnterActivity.this, item.getText(), toast.LENGTH_SHORT).show();
-        toast.makeText(ManualEnterActivity.this, location.getText(), toast.LENGTH_SHORT).show();
-        toast.makeText(ManualEnterActivity.this, price.getText(), toast.LENGTH_SHORT).show();
+        areaDisplay = (TextView) findViewById(R.id.areaDisplay);
+    }
+
+    //save entered text to button saveInfo
+        public void saveInfo (View view){
+        SharedPreferences sharedPref = getSharedPreferences ("itemInfo", Context.MODE_PRIVATE );
+
+        SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putString("itemName", item.getText().toString());
+            editor.putString("locName", location.getText().toString());
+            editor.putString("priceName", price.getText().toString());
+            editor.apply();
+
+            Toast.makeText(this, "Saved!", Toast.LENGTH_LONG).show();
+        }
+
+        //button to test to display saved data
+        public void displayInfo (View view){
+            SharedPreferences sharedPref = getSharedPreferences ("itemInfo", Context.MODE_PRIVATE);
+
+            String iname = sharedPref.getString("itemName", "");
+            String lname = sharedPref.getString("locName", "");
+            String pname = sharedPref.getString("priceName", "");
+            areaDisplay.setText(iname + " " + lname + " " + pname);
+
+
+
+
+
+    }
+
+
+    public void buttonOnClick(View v) {
+
     }
 }
 
