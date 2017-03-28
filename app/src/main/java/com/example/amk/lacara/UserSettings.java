baf260be70ae5846252e600d68acc305f865b4ad
L.J. Imutan <l_imutan1@u.pacific.edu>
2017-03-28 09:31:59 -0700

    Adding SharedPreferences to UserSettings

--- app/src/main/java/com/example/amk/lacara/UserSettings.java
+++ app/src/main/java/com/example/amk/lacara/UserSettings.java
@@ -1,13 +1,31 @@
 package com.example.amk.lacara;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class UserSettings extends AppCompatActivity {
+
+// add input variables here
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_settings);
+
+        //add inputs here
     }
+
+//save user settings button
+    public void saveInfo (View view){
+        SharedPreferences sharedPref = getSharedPreferences("userSettings", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+    }
+
 }
