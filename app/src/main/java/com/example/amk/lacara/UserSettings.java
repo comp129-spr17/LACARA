d06856d290c5ac2caec9cea3b40ad8f0c6b40178
L.J. Imutan <l_imutan1@u.pacific.edu>
2017-04-02 19:37:00 -0700

    Added a Toggle Button to switch from Dark/Light Theme

--- app/src/main/java/com/example/amk/lacara/UserSettings.java
+++ app/src/main/java/com/example/amk/lacara/UserSettings.java
@@ -2,23 +2,30 @@ package com.example.amk.lacara;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.RelativeLayout;
+import android.widget.ToggleButton;
 
-public class UserSettings extends AppCompatActivity {
+public class UserSettings extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener{
 
 // add input variables here
 
 
-
+ToggleButton t;
+RelativeLayout r;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_settings);
+        t=(ToggleButton)findViewById(R.id.theme);
+        r= (RelativeLayout) findViewById(R.id.layout);
 
-        //add inputs here
+        t.setOnCheckedChangeListener(this);
     }
 
 //save user settings button
@@ -28,4 +35,15 @@ public class UserSettings extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean t) {
+        if(t)
+        {
+            r.setBackgroundColor(Color.BLACK);
+        }
+        else
+        {
+            r.setBackgroundColor(Color.WHITE);
+        }
+    }
 }
