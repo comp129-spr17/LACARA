fd4aef7353428f87358b6f6ce9594a7c993c64d8
L.J. Imutan <l_imutan1@u.pacific.edu>
2017-04-03 17:47:31 -0700

    Fixed up the UserSettings

--- app/src/main/java/com/example/amk/lacara/UserSettings.java
+++ app/src/main/java/com/example/amk/lacara/UserSettings.java
@@ -9,12 +9,17 @@ import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.RelativeLayout;
 import android.widget.ToggleButton;
+import android.support.v7.widget.Toolbar;
+
+
+
+
 
 public class UserSettings extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener{
 
 // add input variables here
 
-
+int dark = Color.parseColor("#7A8AA1");
 ToggleButton t;
 RelativeLayout r;
 
@@ -22,6 +27,8 @@ RelativeLayout r;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_settings);
+        Toolbar SettingsToolbar = (Toolbar) findViewById(R.id.settingsTB);
+        setSupportActionBar(SettingsToolbar);
         t=(ToggleButton)findViewById(R.id.theme);
         r= (RelativeLayout) findViewById(R.id.layout);
 
@@ -39,11 +46,11 @@ RelativeLayout r;
     public void onCheckedChanged(CompoundButton buttonView, boolean t) {
         if(t)
         {
-            r.setBackgroundColor(Color.BLACK);
+            r.setBackgroundColor(Color.WHITE);
         }
         else
         {
-            r.setBackgroundColor(Color.WHITE);
+            r.setBackgroundColor(Color.parseColor("#7A8AA1 "));
         }
     }
 }
