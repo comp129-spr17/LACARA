739b8f56140e7a3cd1413b715cb34100b6193631
Ming Yan Shu <Ming Yan Shu>
2017-04-09 01:36:55 -0700

    Add Graph View

--- app/src/main/java/com/example/amk/lacara/GraphsActivity.java
+++ app/src/main/java/com/example/amk/lacara/GraphsActivity.java
@@ -3,11 +3,57 @@ package com.example.amk.lacara;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class GraphsActivity extends AppCompatActivity {
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
+import java.util.ArrayList;
+
+public class GraphsActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graphs);
+
+        BarChart barChart = (BarChart) findViewById(R.id.chart);
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(4f, 0));
+        entries.add(new BarEntry(8f, 1));
+        entries.add(new BarEntry(6f, 2));
+        entries.add(new BarEntry(12f, 3));
+        entries.add(new BarEntry(18f, 4));
+        entries.add(new BarEntry(9f, 5));
+        entries.add(new BarEntry(4f, 6));
+        entries.add(new BarEntry(4f, 7));
+        entries.add(new BarEntry(4f, 8));
+        entries.add(new BarEntry(4f, 9));
+        entries.add(new BarEntry(4f, 10));
+        entries.add(new BarEntry(4f, 11));
+
+        BarDataSet dataset = new BarDataSet(entries, "Your expense");
+
+        ArrayList<String> labels = new ArrayList<String>();
+        labels.add("January");
+        labels.add("February");
+        labels.add("March");
+        labels.add("April");
+        labels.add("May");
+        labels.add("June");
+        labels.add("July");
+        labels.add("August");
+        labels.add("September");
+        labels.add("October");
+        labels.add("November");
+        labels.add("December");
+
+        BarData data = new BarData(labels, dataset);
+        dataset.setColors(ColorTemplate.COLORFUL_COLORS);
+        barChart.setData(data);
+        barChart.animateY(5000);
+        barChart.setDescription("Monthly Expenses");
     }
 }
+
