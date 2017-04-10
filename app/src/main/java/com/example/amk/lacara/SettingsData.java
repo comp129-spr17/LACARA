bbbf5c578ef8c66f955dc1114a8fb3de49211500
aungmkyaw <a_kyaw@u.pacific.edu>
2017-04-09 23:02:16 -0700

    Push

---
+++ app/src/main/java/com/example/amk/lacara/SettingsData.java
@@ -0,0 +1,17 @@
+package com.example.amk.lacara;
+
+/**
+ * Created by AMK on 4/9/2017.
+ */
+
+public class SettingsData {
+
+    private int _id;
+    private int _budget;
+
+    public UserSettingsData(String name, String location, double price)
+    {
+    }
+
+}
+
