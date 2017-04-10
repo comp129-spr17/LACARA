af93ceea92840116b5606610481357d5c239346c
aungmkyaw <a_kyaw@u.pacific.edu>
2017-04-09 23:18:33 -0700

    add

--- app/src/main/java/com/example/amk/lacara/SettingsData.java
+++ app/src/main/java/com/example/amk/lacara/SettingsData.java
@@ -7,21 +7,32 @@ package com.example.amk.lacara;
 public class SettingsData {
 
     private int _id;
-    private int _budget;
-    private String _Name;
-    private String _Password;
-    private int _Notifications;
+    private double _budget;
+    private String _name;
+    private String _password;
+    private int _notifications;
 
 
-    public SettingsData(int id, int budget, String name, String pass, int notif)
+    public SettingsData(int id, double budget, String name, String pass, int notif)
     {
         this._budget=budget;
         this._id=id;
-        this._Name=name;
-        this._Password=pass;
-        this._Notifications=notif;
+        this._name=name;
+        this._password=pass;
+        this._notifications=notif;
+    }
 
+    public double get_budget(){
+        return _budget;
+    }
 
+    public String get_name(){
+        return _name;
+    }
+
+    public int get_notifications()
+    {
+        return _notifications;
     }
 
 }
