f0b968b1c6cba95ee65de3f9eeaa96003e954762
Ming Yan Shu <Ming Yan Shu>
2017-05-01 18:39:50 -0700

    piechart is done

--- app/src/main/java/com/example/amk/lacara/GraphsActivity.java
+++ app/src/main/java/com/example/amk/lacara/GraphsActivity.java
@@ -4,9 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
@@ -20,8 +24,9 @@ public class GraphsActivity extends AppCompatActivity{
         setContentView(R.layout.activity_graphs);
         db = new MyDBHandler(this, null, null, 1);
         ArrayList<BarEntry> entries = MonthlyData(db);
+        ArrayList<Entry> entriesp = MonthlyCat(db);
 
-        BarChart barChart = (BarChart) findViewById(R.id.chart);
+        BarChart barChart = (BarChart) findViewById(R.id.chartb);
 
         BarDataSet dataset = new BarDataSet(entries, "Your monthly expenses");
 
@@ -42,8 +47,25 @@ public class GraphsActivity extends AppCompatActivity{
         BarData data = new BarData(labels, dataset);
         dataset.setColors(ColorTemplate.COLORFUL_COLORS);
         barChart.setData(data);
-        barChart.animateY(3000);
+        barChart.animateY(1000);
         barChart.setDescription("Monthly Expenses");
+
+        PieChart pieChart = (PieChart) findViewById(R.id.chartp);
+        PieDataSet datap = new PieDataSet(entriesp, "Categories of expenses");
+        ArrayList<String> label2 = new ArrayList<String>();
+        label2.add("Food");
+        label2.add("Utilities");
+        label2.add("Personal");
+        label2.add("Activities");
+        label2.add("Auto");
+        label2.add("Home");
+        PieData pd = new PieData(label2, datap);
+        datap.setColors( ColorTemplate.COLORFUL_COLORS);
+        pieChart.setData(pd);
+        pieChart.animateY(1000);
+        pieChart.setDescription("Item categories break down chart by year");
+
+
     }
     public ArrayList<BarEntry> MonthlyData(MyDBHandler DB)
     {
@@ -77,6 +99,48 @@ public class GraphsActivity extends AppCompatActivity{
 
         return Entries;
     }
+
+    public ArrayList<Entry> MonthlyCat(MyDBHandler db){
+        ArrayList<Entry> d = new ArrayList<>();
+        String yr = String.valueOf(Calendar.getInstance().get(Calendar.YEAR));
+        float monthlyFood = 0;
+        float monthlyU = 0;
+        float MonthlyP = 0;
+        float mAct= 0;
+        float mAuto = 0;
+        float mHome = 0;
+        ArrayList<String> Month = new ArrayList<String>();
+        Month.add("01");
+        Month.add("02");
+        Month.add("03");
+        Month.add("04");
+        Month.add("05");
+        Month.add("06");
+        Month.add("07");
+        Month.add("08");
+        Month.add("09");
+        Month.add("10");
+        Month.add("11");
+        Month.add("12");
+        for(int i = 0; i < Month.size(); i++)
+        {
+            monthlyFood += (float) db.getMonthCat(Month.get(i), yr, "Food");
+            monthlyU += (float) db.getMonthCat(Month.get(i), yr, "Utilities");
+            MonthlyP += (float) db.getMonthCat(Month.get(i), yr, "Personal");
+            mAct += (float) db.getMonthCat(Month.get(i), yr, "Activities");
+            mAuto += (float) db.getMonthCat(Month.get(i), yr, "Auto");
+            mHome += (float) db.getMonthCat(Month.get(i), yr, "Home");
+        }
+        d.add(new Entry(monthlyFood, 0));
+        d.add(new Entry(monthlyU, 1));
+        d.add(new Entry(MonthlyP, 2));
+        d.add(new Entry(mAct, 3));
+        d.add(new Entry(mAuto, 4));
+        d.add(new Entry(mHome, 5));
+
+        return d;
+    }
+
 
 }
 
